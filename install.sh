af2a8b63dad22b5ba55dc055089f71b93eef5e80
Sam Kasman <sk@samkasman.com>
2019-11-19 17:26:17 -0500

    Disable macOS application resume

--- install.sh
+++ install.sh
@@ -33,6 +33,9 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
+# Disable Resume system-wide
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+
 
 # XCode
 # --
