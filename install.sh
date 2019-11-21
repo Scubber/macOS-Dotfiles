1e721fe59439f42a39cbc63c99c1a2e9724b6ae0
Sam Kasman <sk@samkasman.com>
2019-11-21 17:36:20 -0500

    Re-enabled some disabled install steps

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
-# ##
-# ## Let's Go!
-# ##
+#
+# Let's Go!
+#
 
-# # Welcome message
-# # --
-# printf "\e[96m"
-# echo "\\n--"
-# echo "Greetings, traveler!"
-# echo "--"
-# echo "Welcome to @samkasman's base macOS workstation configuration."
-# echo "--"
-# echo "https://samkasman.com"
-# echo "--"
-# echo "WARNING: Be sure you know exactly what this is going to do. You might lose some important settings."
-# echo "--\\n"
-# printf "\e[0m"
+# Welcome message
+# --
+printf "\e[96m"
+echo "\\n--"
+echo "Greetings, traveler!"
+echo "--"
+echo "Welcome to @samkasman's base macOS workstation configuration."
+echo "--"
+echo "https://samkasman.com"
+echo "--"
+echo "WARNING: Be sure you know exactly what this is going to do. You might lose some important settings."
+echo "--\\n"
+printf "\e[0m"
 
-# # Main stuff
-# # --
+# Main stuff
+# --
 
-# # make scripts in configs/ executable
-# echo "\\nPreparing for launch.\\n"
-# chmod -R +x ./configs
+# make scripts in configs/ executable
+echo "\\nPreparing for launch.\\n"
+chmod -R +x ./configs
 
-# echo "\\nInstall...\\n"
+echo "\\nInstall...\\n"
 
 # script install menu
 PS3=""
@@ -47,35 +47,36 @@ do
         "System Preferences")
             echo "\\n$opt, comin' right up!"
 
-			./configs/macOS/macOS.sh
-
 			sudo -v
+
+			./configs/macOS/macOS.sh
 
             break;;
         "Applications")
             echo "\\n$opt, comin' right up!"
 
-			./configs/brew/brew.sh
-
 			sudo -v
+
+			./configs/brew/brew.sh
 
             break;;
 		"CLI Tools")
             echo "\\n$opt, comin' right up!"
 
+			sudo -v
+
 			./configs/zsh/zsh.sh
-			# backup/replace shell scripts here
 			./configs/git/git.sh
 
-			sudo -v
+			# backup/replace shell scripts here
 
             break;;
 		"Ableton Live")
             echo "\\n$opt, comin' right up!"
 
-			./configs/ableton/ableton.sh
-
 			sudo -v
+
+			./configs/ableton/ableton.sh
 
             break;;
         "Quit")
